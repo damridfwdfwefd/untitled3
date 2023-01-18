5c85e4ebd1f48ed8577e703fbfd2359c0f9a98f3
damridfwdfwefd <alexdamsem2004@gmail.com>
2023-01-18 21:27:48 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import.io.IOExcamption
+import.nio.file.Files
+import
+
+import java.nio.file.Files;
+import java.nio.file.Paths;.nio.file.Paths
+
+public class Main {
+    public static void main(String[] args) {
+        throws IOException
+        {
+            String string = new String (Files.readAllBytes(Paths.get("C:\Users\user\Downloads\\натуральные числа
+            String string = new String.split( )"\\s\\W);"
+         int cout = 0
+                for(int i)
+        }
+
+}
